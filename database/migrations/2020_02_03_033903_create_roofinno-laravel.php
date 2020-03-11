1c072e92a20cb6aea3b7398d6abeb4c8d4a136a3
cruseraca <syahrianadil22@gmail.com>
2020-03-11 14:52:58 +0700

    m

---
+++ database/migrations/2020_02_03_033903_create_roofinno-laravel.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoofinnoLaravel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_inout', function (Blueprint $table) {
+            $table->increments('IDINOUT');
+            $table->integer('IDSENSOR');
+            $table->string('POWER',50);
+            $table->string('VOLTAGE',50);
+            $table->string('CURRENT',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_inout');
+    }
+}
