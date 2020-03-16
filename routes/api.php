0e4c998efbbb70384fccf496cd3b729b5a9d2f90
cruseraca <syahrianadil22@gmail.com>
2020-03-16 15:17:23 +0700

    hardware sync

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\KonsumsiData;
+use App\Data;
+use Carbon\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,31 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/esp8266/post','HardwareController@postData');
+Route::get('/esp8266/post',function(Request $request){
+    $timestamp = Carbon::now('Asia/Jakarta')->format('Y-m-d H:i:s');
+    $data = new Data;
+    $data->IDUSER = $request->IDUSER;
+    $data->POWER_PS = $request->POWER_PS;
+    $data->POWER_LOAD = $request->POWER_LOAD;
+    $data->ONINSERT = $timestamp;
+    $data->timestamps = false;
+
+    $data->save();
+    return "OK";
+});
+// Route::get('/esp8266/post_kons','HardwareController@postKons');
+Route::get('/esp8266/post_kons',function (Request $request){
+    $timestamp = Carbon::now('Asia/Jakarta')->format('Y-m-d H:i:s');
+    $data = new KonsumsiData;
+    $data->IDUSER = $request->IDUSER;
+    $data->IDSENSOR = $request->IDSENSOR;
+    $data->POWER = $request->POWER;
+    $data->ONINSERT = $timestamp;
+    $data->timestamps = false;
+
+    $data->save();
+    return "OK";
 });
