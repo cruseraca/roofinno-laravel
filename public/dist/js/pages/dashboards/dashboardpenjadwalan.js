8acb7fd608ed525922cb7b350a736b9130446ad1
wildanadt <osolott98@gmail.com>
2020-03-22 12:43:47 +0700

    fixing Dashboard penjadwalan

--- public/dist/js/pages/dashboards/dashboardpenjadwalan.js
+++ public/dist/js/pages/dashboards/dashboardpenjadwalan.js
@@ -29,35 +29,35 @@ $(function () {
             chartMonitorKons = new Chart(ctxChart, {
                 type: 'line',
                 data: {
-                    labels: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '00:00'],
+                    labels: dataKonsumsi[0],
                     datasets: [
                         {
                             label: 'Beban 1',
                             data: dataKonsumsi.konsumsi_data[0].konsumsi_day1,
                             borderColor: '#cc0000',
                             backgroundColor: "rgba(204,0,0,0.25)",
-                            borderWidth: 2,
+                            borderWidth: 1,
                         },
                         {
                             label: 'Beban 2',
                             data: dataKonsumsi.konsumsi_data[0].konsumsi_day2,
                             borderColor: '#00cc00',
                             backgroundColor: "rgba(0,204,0,0.25)",
-                            borderWidth: 2,
+                            borderWidth: 1,
                         },
                         {
                             label: 'Beban 3',
                             data: dataKonsumsi.konsumsi_data[0].konsumsi_day3,
                             borderColor: '#8A2BE2',
                             backgroundColor: "rgba(138,43,226,0.25)",
-                            borderWidth: 2,
+                            borderWidth: 1,
                         },
                         {
-                            label: 'Beban 4',
+                            label: 'Beban Total',
                             data: dataKonsumsi.konsumsi_data[0].konsumsi_day_total,
                             borderColor: '#2962FF',
                             backgroundColor: "rgba(41,98,255,0.25)",
-                            borderWidth: 2,
+                            borderWidth: 1,
                         }
                     ]
                 },
@@ -70,7 +70,7 @@ $(function () {
                                 autoSkip: true,
                                 maxRotation: 0,
                                 callback: function (value, index, values) {
-                                    return value;
+                                    return ' '+value+' ';
                                 }
                             }
                         }],
